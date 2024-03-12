2c0ac859611c855d9a43c2f8a4d96540d32a5bc3
rohantastic <rohantastic@outlook.com>
2024-03-12 23:46:07 +0530

    orderController

---
+++ controllers/orderController.js
@@ -0,0 +1,64 @@
+const orderModel = require('../models/orderModel');
+const cartModel = require('../models/cartModel');
+const productsModel = require('../models/productsModel');
+const userModel = require('../models/userModel');
+
+const createOrder = async (cartId,decodedToken) => {
+    const cartID = cartId;
+    const email = decodedToken.email;
+
+    try {
+        const user = await userModel.findOne({ where: { email } });
+
+        if (!user) {
+            throw new Error('User not found');
+        }
+
+        const userName = user.name;
+        const userID = user.userID;
+
+        const cartItem = await cartModel.findOne({ where: { cartID } });
+
+        if (!cartItem) {
+            throw new Error('Cart item not found');
+        }
+
+        const productID = cartItem.productID;
+
+        const productItem = await productsModel.findOne({ where: { productID } });
+
+        if (!productItem) {
+            throw new Error('Product not found');
+        }
+
+        const productName = productItem.name;
+        const productDescription = productItem.description;
+        const productPrice = productItem.price;
+
+        const result = await orderModel.create({
+            userName,
+            productName,
+            productDescription,
+            productPrice,
+            cartID,
+            userID
+        });
+
+        if (result) {
+            const cartRemoved = await cartModel.destroy({ where: { cartID } });
+        }
+
+        return{
+            orderID: result.orderID,
+            userName:result.userName,
+            productName:result.productName,
+            productDescription:result.productDescription,
+            productPrice: result.productPrice
+        }
+    } catch (e) {
+        console.error(e);
+        throw new Error(e);
+    }
+}
+
+module.exports = { createOrder };
