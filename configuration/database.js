1a6a44b6fc1d11dd36bc2b7738a70bcd62d333e0
rohantastic <rohantastic@outlook.com>
2024-03-13 11:41:54 +0530

    jest installation

--- configuration/database.js
+++ configuration/database.js
@@ -2,7 +2,9 @@ const Sequelize = require('sequelize');
 
 const sequelize = new Sequelize('ecom','root','root',{
     dialect: 'mysql',
-    host: 'localhost'
+    host: 'localhost',
+    charset: 'utf8mb4', 
+    collate: 'utf8mb4_unicode_ci'
 });
 
 
